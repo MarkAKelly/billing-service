77371540cd7a22aca9fe8565fd4f33736d33f5f3
MarkAKelly <21219364+MarkAKelly@users.noreply.github.com>
2019-05-08 16:35:56 +0100

    add paylsip logic

--- src/main/scala/cafe/Tax.scala
+++ src/main/scala/cafe/Tax.scala
@@ -1,19 +1,58 @@
 package cafe
 
+import sun.security.krb5.internal.PAData.SaltAndParams
+
 object Tax {
 
+  val allowance:BigDecimal = 11000
+
+  val monthly : BigDecimal => BigDecimal = num => (num / 12).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+
   def getPayslip(salary: BigDecimal): Payslip = {
-    val monthly : BigDecimal => BigDecimal = num => num / 12
+
+    taxPayable(salary)
 
     Payslip(
       grossSalary = monthly(salary),
-      ni = 159.4,
-      allowance = 916.67,
-      taxableIncome = 1083.33,
-      taxPayable = 216.67)
+      ni = monthly(getNi(salary)),
+      allowance = monthly(getAllowance(salary)),
+      taxableIncome = monthly(taxableIncome(salary)),
+      taxPayable = monthly(taxPayable(salary)))
   }
 
-  private def getNI
+  private def getNi(salary: BigDecimal) : BigDecimal = {
+    val basicRate = 0.12
+    val higherRate = 0.02
+
+    val highSalary: BigDecimal = if (salary > 43000) salary - 43000 else 0
+    val basicSalary: BigDecimal = if (salary > 8060) salary - 8060 - highSalary else 0
+
+    (highSalary * higherRate) + (basicSalary * basicRate)
+  }
+
+  private def getAllowance(salary: BigDecimal) = {
+    allowance
+  }
+
+  private def taxableIncome(salary: BigDecimal) = {
+    salary - allowance
+  }
+
+  private def taxPayable(salary: BigDecimal): BigDecimal = {
+
+    val basic = 0.2
+    val higher = 0.4
+    val additional = 0.45
+
+    val taxable = taxableIncome(salary)
+    val additionalAmt: BigDecimal = if (taxable > 155000) taxable - 155000 else 0
+    val higherAmt: BigDecimal = if (taxable > 43000) taxable - 11000 - additionalAmt else 0
+    val basicAmt: BigDecimal = if (taxable > 11000) taxable - higherAmt - higherAmt   else 0
+
+    (basicAmt * basic) + (higherAmt * higher) + (additionalAmt * additional)
+
+  }
+
 
 }
 
